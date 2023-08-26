6c3977075c2b5b4d330187a675f979103307cfc7
MenendezON <menendezon@gmail.com>
2023-08-26 19:46:05 +0000

    Update src folder

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Category from './routes/Category';
 import Header from './components/Header';
-import API from './components/API';
 
 function App() {
   return (
     <>
       <Header />
-      <API/>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
